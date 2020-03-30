3de6c7db1ab8c1adcc7e484a414e5c86f7a0507a
TiagoSeixas2103 <62569082+TiagoSeixas2103@users.noreply.github.com>
2020-03-30 09:57:45 -0300

    Update PassLineBet.git.py

--- PassLineBet.git.py
+++ PassLineBet.git.py
@@ -2,6 +2,7 @@ from random import randint
 fichas = 100
 programa = True
 while fichas > 0 and programa == True:
+    programa2 = True
     pergunta = input("Vai apostar ou sair? ")
     if pergunta != "sair":
         fase = "Come Out"
@@ -14,10 +15,12 @@ while fichas > 0 and programa == True:
         Twelve = int(input("Aposta quanto na Twelve? "))
         if Pass_Line_Bet>0 and Field>=0 and Any_Craps>=0 and Twelve>=0:
             if dado1 + dado2 == 2:
+                fichas -= Pass_Line_Bet
                 fichas += Field*2
                 fichas += Any_Craps*7
                 fichas -= Twelve
             elif dado1 + dado2 == 3:
+                fichas -= Pass_Line_Bet
                 fichas += Field
                 fichas += Any_Craps*7
                 fichas -= Twelve
@@ -25,15 +28,72 @@ while fichas > 0 and programa == True:
                 fichas += Field
                 fichas -= Any_Craps
                 fichas -= Twelve
-            elif dado1+dado2 == 5 or 6 or 7 or 8:
+                Point = dado1+dado2
+                fase = "Point"
+                print (fase)
+                while programa2==True:
+                    dado1 = randint(1,6)
+                    dado2 = randint(1,6)
+                    Field = int(input("Aposta quanto na Field? "))
+                    Any_Craps = int(input("Aposta quanto na Any Craps? "))
+                    Twelve = int(input("Aposta quanto na Twelve? "))
+                    if dado1 + dado2 == 2:
+                        fichas += Field*2
+                        fichas += Any_Craps*7
+                        fichas -= Twelve
+                    elif dado1 + dado2 == 3:
+                        fichas += Field
+                        fichas += Any_Craps*7
+                        fichas -= Twelve
+                    elif dado1 + dado2 == 4:
+                        fichas += Pass_Line_Bet
+                        fichas += Field
+                        fichas -= Any_Craps
+                        fichas -= Twelve
+                        programa2 = False
+                    elif dado1+dado2 == 5 or 6 or 8:
+                        fichas -= Field
+                        fichas -= Any_Craps
+                        fichas -= Twelve
+                    elif dado1+dado2 == 7:
+                        fichas -= Pass_Line_Bet
+                        fichas -= Field
+                        fichas -= Any_Craps
+                        fichas -= Twelve
+                        programa2 = False
+                    elif dado1 + dado2 == 9 or 10:
+                        fichas += Field
+                        fichas -= Any_Craps
+                        fichas -= Twelve
+                    elif dado1 + dado2 == 11:
+                        fichas += Field
+                        fichas -= Any_Craps
+                        fichas -= Twelve
+                    elif dado1 + dado2 == 12:
+                        fichas += Field*3
+                        fichas += Any_Craps*7
+                        fichas += Twelve*30
+            elif dado1+dado2 == 5 or 6 or 8:
                 fichas -= Field
                 fichas -= Any_Craps
                 fichas -= Twelve
-            elif dado1 + dado2 == 9 or 10 or 11:
+                Point = dado1+dado2
+                fase = "Point"
+                print (fase)
+                
+            elif dado1+dado2 == 7:
+                fichas += Pass_Line_Bet
+                fichas -= Field
+                fichas -= Any_Craps
+                fichas -= Twelve
+
+            elif dado1 + dado2 == 11:
+                fichas += Pass_Line_Bet
                 fichas += Field
                 fichas -= Any_Craps
                 fichas -= Twelve
             elif dado1 + dado2 == 12:
+                fichas -= Pass_Line_Bet
                 fichas += Field*3
                 fichas += Any_Craps*7
                 fichas += Twelve*30
@@ -62,3 +122,8 @@ while fichas > 0 and programa == True:
                 fichas += Field*3
                 fichas += Any_Craps*7
                 fichas += Twelve*30
+        else:
+            print ("Você digitou uma das apostas como sendo um valor negativo, tente novamente")
+    else:
+        print ("Obrigado por jogar")
+        programa = False
